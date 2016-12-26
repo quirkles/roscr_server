f624fb5c364e264b688c84d260794d4c7f450bfb
quirkles <al.quirk@gmail.com>
2016-12-25 21:21:26 -0500

    Comment out unused vars

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -2,7 +2,7 @@ const user = 'default-user';
 const pass = 'change-this-password';
 
 export const uri = process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb://127.0.0.1:27017/myapp';
-export const opts = {
-  user,
-  pass
-};
+// export const opts = {
+//   user,
+//   pass
+// };
