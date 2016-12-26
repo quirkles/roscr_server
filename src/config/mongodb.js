d8930992b1295e0d7715f956074b62a4d26e9608
quirkles <al.quirk@gmail.com>
2016-12-25 21:03:12 -0500

    Use heroku mongodb uri if exists

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,8 +1,7 @@
 const user = 'default-user';
 const pass = 'change-this-password';
 
-export const uri = 'mongodb://127.0.0.1:27017/myapp';
-
+export const uri = process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb://127.0.0.1:27017/myapp';
 export const opts = {
   user,
   pass
