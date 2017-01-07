ceccc20c79511794842c351c62ad7d14ba345416
quirkles <al.quirk@gmail.com>
2017-01-07 13:10:15 -0500

    User seed script done

--- scripts/seed/seed_users.js
+++ scripts/seed/seed_users.js
@@ -1,9 +1,18 @@
 import mongoose from 'mongoose';
 import {uri} from '../../src/config/mongodb';
 
+import user_model from '../../src/models/user_model';
+
 import {generate_random_user} from '../../src/utils/user';
 
 const db = mongoose.connect(uri);
 
+const generate_users = n => Array.from({length: n}, generate_random_user);
 
-
+user_model.create(generate_users(100), (err, circle) => {
+  if (err) {
+    console.log(err);
+  } else {
+    db.disconnect();
+  }
+});
